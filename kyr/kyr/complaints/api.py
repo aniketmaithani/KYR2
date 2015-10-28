b2cea65f949e8df9401b9ed6419da309fea14228
Aniket Maithani <maithani.aniket@gmail.com>
2015-10-29 05:04:32 +0530

    feat(complaints/api): added `ListModelMixin`

--- kyr/kyr/complaints/api.py
+++ kyr/kyr/complaints/api.py
@@ -4,11 +4,12 @@ from __future__ import absolute_import, unicode_literals
 # Third Party Stuff
 from .models import Complaints
 from .serializers import ComplaintSerializer
+from rest_framework.decorators import api_view
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
 
 
-class ComplaintViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+class ComplaintViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet, mixins.ListModelMixin):
 
     """
     API endpoint that allows user to submit their complaints 
