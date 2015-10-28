0748dcbde0723a865d1172183adcace8df85ed61
Aniket Maithani <maithani.aniket@gmail.com>
2015-10-29 04:24:29 +0530

    feat(complaints/): added the following - serializers and api modules added for complaints app - DRF added - settings modified for the same - `/complaints` endpoint in progress

--- kyr/kyr/settings.py
+++ kyr/kyr/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kyr.complaints', # Complaints Application
+    'rest_framework', # Django Rest Framework
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest Framework Settings 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
+
--- kyr/kyr/urls.py
+++ kyr/kyr/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from complaints.api import ComplaintViewSet	
+
+router = routers.DefaultRouter(trailing_slash=True)
+router.register(r'complaints', ComplaintViewSet, base_name="complaints")
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
