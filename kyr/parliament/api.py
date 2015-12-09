1bb1d3cd4277388bd9961b3f33a5782f54061881
aniketmaithani <maithani.aniket@gmail.com>
2015-12-10 01:48:45 +0530

    feat(parliament/api): changed permission to `allowAny`

--- kyr/parliament/api.py
+++ kyr/parliament/api.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+# Third Party Stuff
 from .models import MemberOfParliament
 from rest_framework import viewsets
 from .serializers import MemberOfParliamentSerializer
+from rest_framework.permissions import AllowAny
 
 
 class MemberOfParliamentViewSet(viewsets.ModelViewSet):
@@ -9,5 +14,6 @@ class MemberOfParliamentViewSet(viewsets.ModelViewSet):
     API Endpoint that allows MP to be viewed or edited
     '''
 
+    permission_classes = (AllowAny, )
     queryset = MemberOfParliament.objects.all().order_by('name_of_the_mp')
     serializer_class = MemberOfParliamentSerializer
