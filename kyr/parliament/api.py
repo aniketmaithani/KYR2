22beb5195f970ed4fba3c9ee36cba079b3b79c3d
aniketmaithani <maithani.aniket@gmail.com>
2015-12-10 18:27:16 +0530

    feat(parliament/): added only GET method to endpoint - added django-extension in requirements.txt - removed UUID field in parliament models.py

--- kyr/parliament/api.py
+++ kyr/parliament/api.py
@@ -6,6 +6,7 @@ from .models import MemberOfParliament
 from rest_framework import viewsets
 from .serializers import MemberOfParliamentSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 
 class MemberOfParliamentViewSet(viewsets.ModelViewSet):
@@ -17,3 +18,8 @@ class MemberOfParliamentViewSet(viewsets.ModelViewSet):
     permission_classes = (AllowAny, )
     queryset = MemberOfParliament.objects.all().order_by('name_of_the_mp')
     serializer_class = MemberOfParliamentSerializer
+
+    def retrieve(self, request, pk=None):
+        queryset = MemberOfParliament.objects.all().order_by('name_of_the_mp')
+        serializer = MemberOfParliamentSerializer(queryset)
+        return Response(serializer.data)
