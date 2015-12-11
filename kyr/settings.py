ee62f124d46c4da6e6c81d6990058bba254203bf
aniketmaithani <maithani.aniket@gmail.com>
2015-12-11 14:19:35 +0530

    feat(settings.py): moved to postgresql, settings added

--- kyr/settings.py
+++ kyr/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'kyr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kyr',
+        'USER': 'db_user',
+        'PASSWORD': 'db_user_password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Internationalization
